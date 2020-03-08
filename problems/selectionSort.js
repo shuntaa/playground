5cbc853e7c1167a5d7ab8b66bbadf4cf9fa10c1e
Shunta <shunta@doi-air.local>
2020-03-08 16:49:53 +0900

    Refacter findMine

--- problems/selectionSort.js
+++ problems/selectionSort.js
@@ -1,16 +1,19 @@
 function selectionSort(array) {
-  var minIndex;
-  var temp;
   for (var i = 0; i < array.length; i++) {
-    minIndex = i;
-    for (var j = i+1; j < array.length; j++) {
-      if (array[minIndex] > array[j] ) {
-        minIndex = j;
-      }
-    }
+    var minIndex = findMin(array, i);
     swap(array, i, minIndex);
   }
   return array;
+}
+
+function findMin(array, from) {
+  var minIndex = from;
+  for (var j = from +1; j < array.length; j++) {
+    if (array[minIndex] > array[j] ) {
+      minIndex = j;
+    }
+  }
+  return minIndex;
 }
 
 function swap(array, i, j) {
